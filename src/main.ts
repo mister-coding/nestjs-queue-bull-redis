a376b42a49cc3f513bcc02872195c778f4940a97
agik-setiawan <setiawanagik@gmail.com>
2022-11-03 06:18:10 +0700

    add bull board

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { createBullBoard } from '@bull-board/api';
+import { ExpressAdapter, } from '@bull-board/express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { Queue } from 'bull';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/bull-admin');
+  const aQueue = app.get<Queue>(
+    `BullQueue_test-queue`
+  )
+  createBullBoard({
+    queues: [
+      new BullMQAdapter(aQueue)
+    ],
+    serverAdapter,
+  })
+  app.use(
+    '/bull-admin',
+    serverAdapter.getRouter()
+  );
+
   await app.listen(3000);
 }
 bootstrap();
